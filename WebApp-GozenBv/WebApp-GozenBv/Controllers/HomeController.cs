f3ab0edb85fe38b796ca4e004cbff0a11e2bd0ea
GencayTuran <gencay.turan@hotmail.com>
2022-11-02 15:32:02 +0100

    alerts on homecontroller

--- WebApp-GozenBv/WebApp-GozenBv/Controllers/HomeController.cs
+++ WebApp-GozenBv/WebApp-GozenBv/Controllers/HomeController.cs
@@ -5,21 +5,38 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using WebApp_GozenBv.Models;
+using WebApp_GozenBv.Data;
+using WebApp_GozenBv.ViewModels;
 
 namespace WebApp_GozenBv.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly DataDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DataDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            List<string> lstAlerts = new List<string>();
+            var cars = _context.WagenPark.Include(w => w.Firma);
+            foreach (var car in cars)
+            {
+                if ( DateTime.Now >= car.KeuringDate.AddMonths(11))
+                {
+                    int daysLeft = (car.KeuringDate - DateTime.Now).Days;
+                    lstAlerts.Add( car.Id + ". " + car.LicencePlate + " (" + car.Brand + " - " + car.Model + ") KEURING BINNEN " + daysLeft + " DAGEN!");
+                }
+            }
+
             return View();
         }
 
---
+++ WebApp-GozenBv/WebApp-GozenBv/ViewModels/HomeIndexVM.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp_GozenBv.ViewModels
+{
+    public class HomeIndexVM
+    {
+        public string Alert { get; set; }
+    }
+}
