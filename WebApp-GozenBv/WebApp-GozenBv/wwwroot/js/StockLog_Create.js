a3f3c9f0e1f125114717a095c1e3aac2b0cd5e86
gencay.turan@student.pxl.be <gencay.turan@student.pxl.be>
2022-11-26 22:54:27 +0100

    adding removing Rows of OrderItems

--- WebApp-GozenBv/WebApp-GozenBv/wwwroot/js/StockLog_Create.js
+++ WebApp-GozenBv/WebApp-GozenBv/wwwroot/js/StockLog_Create.js
@@ -1,30 +1,30 @@
-﻿let btnAdd = document.getElementById('btnAdd');
-let btnRemove = document.querySelector('.btnRemove');
-let row = document.querySelector('#trStock');
-let clone = row.cloneNode(true); //true means to clone the nested elements as well.
+﻿////let btnAdd = document.getElementById('btnAdd');
+////let btnRemove = document.querySelector('.btnRemove');
+////let row = document.querySelector('#trStock');
+////let clone = row.cloneNode(true); //true means to clone the nested elements as well.
 
-function AddNewRow {
-    clone.ClassName = 'productRow';
-    row.after(clone);
-}
+////function AddNewRow {
+////    clone.ClassName = 'productRow';
+////    row.after(clone);
+////}
 
-function RemoveRow{
-    //remove selected button its tr head
-    btnRemove.parentElement.parentElement.remove();
-}
+////function RemoveRow{
+////    //remove selected button its tr head
+////    btnRemove.parentElement.parentElement.remove();
+////}
 
-btnAdd.addEventListener('click', AddNewRow);
-btnRemove.addEventListener('click', RemoveRow);
-
-
-//get view data to controller
-let selectedProducts = [];
-//let countCells = document.querySelectorAll'productRow').length;
-//let x = document.getElementById('SelectedProducts').rows[]
+////btnAdd.addEventListener('click', AddNewRow);
+////btnRemove.addEventListener('click', RemoveRow);
 
 
+//////get view data to controller
+////let selectedProducts = [];
+//////let countCells = document.querySelectorAll'productRow').length;
+//////let x = document.getElementById('SelectedProducts').rows[]
 
-selectedProducts.forEach()
+
+
+////selectedProducts.forEach()
 
 
 
