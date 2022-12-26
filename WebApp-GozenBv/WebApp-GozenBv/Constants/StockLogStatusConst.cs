5bc06c88c5b8909ebf727b31223cb5908c137308
Gencay Turan <gencay.turan@nara.to>
2022-12-26 16:21:54 +0100

    added status constants

---
+++ WebApp-GozenBv/WebApp-GozenBv/Constants/StockLogStatusConst.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp_GozenBv.Constants
+{
+    public class StockLogStatusConst
+    {
+        public int Complete = 1;
+        public int Incomplete = 0;
+        public int Damaged = 00;
+
+    }
+}
