a3b18ad296dc64724507ebc43d7b5236a9049dab
Gencay Turan <gencay.turan@hotmail.com>
2023-01-10 13:27:24 +0100

    added constants & user model

---
+++ WebApp-GozenBv/WebApp-GozenBv/Constants/ActionsConst.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApp_GozenBv.Constants
+{
+    public static class ActionsConst
+    {
+        public const int Create = 0;
+        public const int Delete = 1;
+        public const int Undo = 2;
+        public const int Edit = 3;
+
+        public const int CompleteReturn = 10;
+        public const int CompleteDamaged = 11;
+
+        public const int Login = 100;
+        public const int Logout = 101;
+    }
+}
---
+++ WebApp-GozenBv/WebApp-GozenBv/Constants/ControllerConst.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApp_GozenBv.Constants
+{
+    public class ControllerConst
+    {
+        public const int Stock = 1;
+        public const int StockLog = 2;
+        public const int Employee = 3;
+        public const int Firma = 4;
+        public const int WagenPark = 5;
+        public const int WagenMaintenance = 6;
+    }
+}
