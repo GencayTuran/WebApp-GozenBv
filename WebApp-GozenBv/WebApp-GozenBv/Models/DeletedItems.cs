66bffe5f80f7bb4a18df391bdc204559ebe33019
gencay.turan@student.pxl.be <gencay.turan@student.pxl.be>
2023-01-13 01:29:47 +0100

    added DeletedItems Model

---
+++ WebApp-GozenBv/WebApp-GozenBv/Models/DeletedItems.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApp_GozenBv.Models
+{
+    public class DeletedItems
+    {
+        public int Id { get; set; }
+        public string EnitityId { get; set; }
+        public int ControllerId { get; set; }
+    }
+}
