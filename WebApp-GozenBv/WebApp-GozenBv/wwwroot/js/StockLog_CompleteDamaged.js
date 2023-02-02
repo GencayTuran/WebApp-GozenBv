7410e41067a9bf832c7dee2011c3b7b56b3119aa
Gencay Turan <gencay.turan@hotmail.com>
2023-02-02 12:07:40 +0100

    inserted dynamic validation for CompleteDamaged

--- WebApp-GozenBv/WebApp-GozenBv/wwwroot/js/StockLog_CompleteDamaged.js
+++ WebApp-GozenBv/WebApp-GozenBv/wwwroot/js/StockLog_CompleteDamaged.js
@@ -1,14 +1,20 @@
 ﻿let stockId = document.querySelectorAll(".stockId");
 let repaired = document.querySelectorAll(".inputRepaired");
 let deleted = document.querySelectorAll(".inputDeleted");
-let damaged = document.querySelectorAll(".inputDamaged")
+let damagedAmount = document.querySelectorAll(".damagedAmount")
 
 let result = document.getElementById("completeDamagedResult");
 
 let data = [];
 
-function FillData() {
+function FillData(event) {
     for (let i = 0; i < stockId.length; i++) {
+        let totalAmounts =  Number(repaired[i].value) + Number(deleted[i].value)
+        if (damagedAmount[i].innerHTML != totalAmounts) {
+            alert("given input not equal to total of repaired & deleted");
+            data = [];
+            event.preventDefault();
+        }
             data.push([
                 stockId[i].innerHTML,
                 repaired[i].value == '' ? '0' : repaired[i].value, 
