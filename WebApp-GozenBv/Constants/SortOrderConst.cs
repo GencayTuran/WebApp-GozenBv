e39bef15571967f47a8ecf9dc00325b75d99bb7e
Gencay Turan <gencay.turan@hotmail.com>
2023-02-13 01:47:28 +0100

    fixes constants for filter search stocklog

---
+++ WebApp-GozenBv/Constants/SortOrderConst.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp_GozenBv.Constants
+{
+    public class SortOrderConst
+    {
+        public const int DateDescendingId = 1;
+        public const int DateAscendingId = 2;
+        public const int EmpAzId = 3;
+        public const int EmpZaId = 4;
+
+        public const string DateDescendingName = "Recent Date";
+        public const string DateAscendingName = "Oldest Date";
+        public const string EmpAzName = "A-Z";
+        public const string EmpZaName = "Z-A";
+    }
+}
--- WebApp-GozenBv/Constants/StockLogStatusConst.cs
+++ WebApp-GozenBv/Constants/StockLogStatusConst.cs
@@ -6,5 +6,9 @@
         public const int Returned = 2;
         public const int DamagedAwaitingAction = 3;
 
+        public const string CreatedName = "Created Logs";
+        public const string ReturnedName = "Returned Items";
+        public const string DamagedAwaitingActionName = "Damaged Awaiting Action";
+
     }
 }
