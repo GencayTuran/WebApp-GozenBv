588bb7d05f68071ad4fdb63509f6f2273d365cd3
gencayturan <gencay.turan@nara.to>
2023-03-19 14:37:44 +0100

    js small fix completeddamaged

--- WebApp-GozenBv/wwwroot/js/StockLog_CompleteDamaged.js
+++ WebApp-GozenBv/wwwroot/js/StockLog_CompleteDamaged.js
@@ -7,13 +7,13 @@ let result = document.getElementById("completeDamagedResult");
 
 
 function FillData(event) {
+    let data = [];
     for (let i = 0; i < stockId.length; i++) {
         let totalAmounts = Number(repaired[i].value) + Number(deleted[i].value)
         if (damagedAmount[i].innerHTML != totalAmounts) {
             alert("given input not equal to total of repaired & deleted");
             event.preventDefault();
         }
-        let data = [];
         let damagedStock = new Object();
         damagedStock.stockId = +stockId[i].innerHTML;
         damagedStock.repairedAmount = repaired[i].value == '' ? 0 : +repaired[i].value;
